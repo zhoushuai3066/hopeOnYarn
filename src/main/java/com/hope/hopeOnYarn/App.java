fdad9b63a81b28369a3e1dba9d4071ad70b239ef
zhoushuai <zhoushuai3066@163.com>
2014-08-27 10:51:55 +0800

    周帅的第一次提交

---
+++ src/main/java/com/hope/hopeOnYarn/App.java
@@ -0,0 +1,13 @@
+package com.hope.hopeOnYarn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
